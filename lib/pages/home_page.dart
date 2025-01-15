d983102391cb9b1128bbb48d2fcb5d2d1611620c
Clmokaya <mokmakori@gmail.com>
2025-01-15 11:38:00 +0300

    formatting

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,29 +1,69 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   HomePage();
 
-@override
+  @override
   State<StatefulWidget> createState() {
     return _HomePageState();
-    
   }
-
 }
-class _HomePageState extends State<HomePage>{
-  _HomePageState();
+
+class _HomePageState extends State<HomePage> {
   late double _deviceHeight, _deviceWidth;
+
+  _HomePageState();
 
   @override
   Widget build(BuildContext context) {
-    _deviceHeight= MediaQuery.of(context).size.height;
-    _deviceWidth=MediaQuery.of(context).size.width;
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: _deviceHeight*0.15,
-        title: Text("My Tasks",style: TextStyle(fontSize: 25), ) ,
+        toolbarHeight: _deviceHeight * 0.15,
+        title: Text(
+          "My Tasks",
+          style: TextStyle(fontSize: 25),
+        ),
       ),
-    
+      body: _tasksLists(),
+      floatingActionButton: _addTaskButton(),
     );
+  }
+
+  Widget _tasksLists() {
+    return ListView(
+      children: [
+        ListTile(
+          title: const Text(
+            "Do laundry!",
+            style: TextStyle(decoration: TextDecoration.lineThrough),
+          ),
+          subtitle: Text(DateTime.now().toString()),
+          trailing: const Icon(
+            Icons.check_box_outlined,
+            color: Colors.red,
+          ),
+        ),
+        ListTile(
+          title: const Text(
+            "Learn flutter!",
+            style: TextStyle(decoration: TextDecoration.lineThrough),
+          ),
+          subtitle: Text(DateTime.now().toString()),
+          trailing: const Icon(
+            Icons.check_box_outlined,
+            color: Colors.red,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _addTaskButton() {
+    return FloatingActionButton(onPressed: () {
+      child:
+      const Icon(Icons.add);
+    });
   }
 }
